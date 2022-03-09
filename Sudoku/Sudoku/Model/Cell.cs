a442b67b7776b990ce33821ec993828414e56441
Vlad Golovatyuk <vladd.golovatyuk@gmail.com>
2022-03-09 13:14:58 +0200

    cell model edging

---
+++ Sudoku/Sudoku/Model/Cell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sudoku.Model
+{
+    internal class Cell
+    {
+        private int _value = 0;
+        private readonly List<int> _surmises = new List<int>(9);
+
+        public Cell((int y, int x) coord, int value)
+        {
+            Coordinate = coord;
+            IsGenerated = value != 0;
+            _value = value;
+        }
+
+        public (int CubeIndex, int CellIndex) Coordinate { get; private set; }
+        public int Value {
+            get => _value;
+            set {
+                if (value < 0 || value >= 10) throw new ArgumentOutOfRangeException(nameof(value));
+                
+                _value = value;
+            }
+        }
+        public bool IsGenerated { get; private set; }
+        public List<int> Surmises => IsGenerated ? null : new List<int>(_surmises);
+    }
+}
