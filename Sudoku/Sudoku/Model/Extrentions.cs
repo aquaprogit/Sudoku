ec299551fb4e593163282b58bb032f42769320d1
Vlad Golovatyuk <vladd.golovatyuk@gmail.com>
2022-03-10 13:40:08 +0200

    field model
    -implimented filling dictionary with Cell and appropriate Grid
    -updating corresponding grid with new Cell value in case it changed
    -class 'Extentions" to easier way to work with data

---
+++ Sudoku/Sudoku/Model/Extrentions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Sudoku.Model
+{
+    internal static class Extrentions
+    {
+        public static IEnumerable<T> FindVisualChildren<T>(this DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    var child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T t)
+                    {
+                        yield return t;
+                    }
+                    foreach (var childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+        public static string GetCellContent(this Cell cell)
+        {
+            if (cell == null) return null;
+            string result = "";
+
+            if (cell.Value == 0)
+            {
+                for (int i = 1; i <= 9; i++)
+                {
+                    if (i % 3 == 1)
+                        result += "\n";
+                    if (cell.Surmises.Contains(i))
+                        result += $"{i} ";
+                    else
+                        result += "  ";
+                }
+                return result.Substring(1, result.Length - 1);
+            }
+            else
+                return cell.Value.ToString();
+        }
+    }
+}
---
+++ Sudoku/Sudoku/Model/Field.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Sudoku.Model
+{
+    internal class Field
+    {
+        private Dictionary<Cell, Grid> _cellToGrids = new Dictionary<Cell, Grid>(81);
+
+        public Field(List<Grid> grids)
+        {
+            int cubeIndex = -1;
+            for (int iter = 0; iter < 81; iter++)
+            {
+                int innerIndex = iter % 9;
+                if (innerIndex == 0)
+                    cubeIndex++;
+                Cell cell = new Cell((cubeIndex, innerIndex), 0);
+                cell.PropertyChanged += Cell_PropertyChanged;
+                _cellToGrids.Add(cell, grids[iter]);
+
+            }
+        }
+
+        private void Cell_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Cell cell = (Cell)sender;
+            TextBlock tb = _cellToGrids[cell].FindVisualChildren<TextBlock>().First();
+            tb.Text = cell.GetCellContent();
+            tb.FontSize = cell.Value == 0 ? 15 : 24;
+            tb.Foreground = new SolidColorBrush(Color.FromArgb((byte)(cell.Value == 0 ? 80 : 100), 0, 0, 0));
+        }
+    }
+}
