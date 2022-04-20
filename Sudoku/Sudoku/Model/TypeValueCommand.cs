1f846165350af738e993773dbd9941c50fb75012
Vlad Golovatyuk <vladd.golovatyuk@gmail.com>
2022-04-20 21:27:21 +0300

    command class and interface

---
+++ Sudoku/Sudoku/Model/TypeValueCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace Sudoku.Model
+{
+    internal class TypeValueCommand : ICommand
+    {
+        private Cell _cell;
+        private Cell _previousCell;
+        public TypeValueCommand(Cell cell)
+        {
+            _cell = cell;
+        }
+        public void Execute(int value, bool isSurmise)
+        {
+            _previousCell = _cell.Copy();
+            if (value < 0 || value > 9) throw new ArgumentOutOfRangeException("value");
+            if (_cell.IsGenerated) return;
+
+            if (isSurmise == false)
+                _cell.Value = value;
+            else
+            {
+                if (_cell.Surmises.Contains(value))
+                    _cell.RemoveSurmise(value);
+                else
+                    _cell.AddSurmise(value);
+            }
+        }
+
+        public void Undo()
+        {
+            _cell.Set(_previousCell);
+        }
+    }
+}
