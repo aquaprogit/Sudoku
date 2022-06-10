04414fcc8ef3663e6524e90ff0002159b0671a46
Vlad Golovatyuk <vladd.golovatyuk@gmail.com>
2022-06-10 23:04:25 +0300

    reorganized viewmodel class and added comments

--- Sudoku/Sudoku/Model/UserData/UserViewModel.cs
+++ Sudoku/Sudoku/Model/UserData/UserViewModel.cs
@@ -7,22 +7,23 @@ namespace Sudoku.Model.UserData
 {
     public class UserViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
         private Difficulty _currentDifficulty;
-
+        /// <summary>
+        /// Returns new instance of <see cref="UserViewModel"/> with default initializion and <see cref="User"/> event subscribtion
+        /// </summary>
         public UserViewModel()
         {
             _currentDifficulty = Difficulty.Easy;
             User.Instance.PropertyChanged += Instance_PropertyChanged;
         }
 
-        private void Instance_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == "Info")
-                OnPropertyChanged(nameof(AvarageTime));
-        }
-
+        /// <summary>
+        /// Notifies subscribes that <see cref="UserViewModel"/>'s property changed
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Current selected difficulty
+        /// </summary>
         public Difficulty CurrentDifficulty {
             get { return _currentDifficulty; }
             set {
@@ -31,18 +32,25 @@ namespace Sudoku.Model.UserData
                 OnPropertyChanged(nameof(AvarageTime));
             }
         }
+        /// <summary>
+        /// Avarage time taken by user to solve <see cref="CurrentDifficulty"/> field
+        /// </summary>
         public TimeSpan AvarageTime {
             get {
                 var result = User.Instance.Info.GroupBy(i => i.Difficulty)
                             .Select(g => new {
-                            difficulty = g.Key,
-                            avarage = g.Sum(a => (int)a.Time.TotalSeconds) / g.Count()
-                    });
+                                difficulty = g.Key,
+                                avarage = g.Sum(a => (int)a.Time.TotalSeconds) / g.Count()
+                            });
                 int seconds = result.Count(a => a.difficulty == _currentDifficulty) > 0 ? result.FirstOrDefault(a => a.difficulty == _currentDifficulty).avarage : 0;
                 return TimeSpan.FromSeconds(seconds);
             }
         }
-
+        private void Instance_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Info")
+                OnPropertyChanged(nameof(AvarageTime));
+        }
         private void OnPropertyChanged([CallerMemberName] string name = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
